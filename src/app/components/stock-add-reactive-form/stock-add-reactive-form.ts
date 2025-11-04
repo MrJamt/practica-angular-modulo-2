8493c77be4a44479cdeaf4be7ac42c4b042d4d8e
AdrianMontano528 <jadrian.montanot2011@gmail.com>
2025-11-03 23:36:32 -0400

    Primera implementacion de stock add en modo formulario reactivo

--- src/app/components/stock-add-reactive-form/stock-add-reactive-form.ts
+++ src/app/components/stock-add-reactive-form/stock-add-reactive-form.ts
@@ -1,11 +1,37 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, output, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Stock } from '../../interfaces/stock.interface';
+import { JsonPipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-stock-add-reactive-form',
-  imports: [],
+  imports: [JsonPipe, TitleCasePipe, ReactiveFormsModule],
   templateUrl: './stock-add-reactive-form.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class StockAddReactiveForm {
 
+export class StockAddReactiveForm {
+  reactiveStockAddTitle = signal<string>("Agregar stock reactivo");
+
+  private fb = inject(FormBuilder);
+
+  OnNewStock = output<Stock>();
+
+  public reactiveStockForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    cuantity: [0, [Validators.required, Validators.min(1)]],
+  });
+
+  addStock() {
+    const { name, cuantity } = this.reactiveStockForm.value;
+
+    const newStock: Stock = {
+      id: Math.floor(Math.random() * 100),
+      name: name!,
+      cuantity: cuantity!,
+    };
+
+    this.OnNewStock.emit(newStock);
+    this.reactiveStockForm.reset();
+  }
 }
